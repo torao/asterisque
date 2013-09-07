fc189fb3f4a4723e2588f9ca2290d17e8c02f987
Takami Torao <koiroha@gmail.com>
2013-09-07 17:01:25 +0900

    migrate from kazzla project

--- src/src/main/scala/com/kazzla/asterisk/netty/Netty.scala
+++ src/src/main/scala/com/kazzla/asterisk/netty/Netty.scala
@@ -5,6 +5,14 @@
 */
 package com.kazzla.asterisk.netty
 
+import java.net.SocketAddress
+import javax.net.ssl.SSLContext
+import org.jboss.netty.bootstrap.{ServerBootstrap, ClientBootstrap, Bootstrap}
+import scala.concurrent.{Promise, Future}
+import com.kazzla.asterisk.Wire
+import org.jboss.netty.channel.socket.nio.{NioServerSocketChannelFactory, NioClientSocketChannelFactory}
+import org.jboss.netty.channel.{ChannelFuture, ChannelFutureListener}
+
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Netty
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -12,4 +20,40 @@ package com.kazzla.asterisk.netty
  * @author Takami Torao
  */
 object Netty {
+
+	def connect(address:SocketAddress, ssl:Option[SSLContext]):(Bootstrap, Future[Wire]) = {
+		logger.trace(s"connect(${address.getName},$ssl)")
+		val promise = Promise[Wire]()
+		val channelFactory = new NioClientSocketChannelFactory()
+		val client = new ClientBootstrap(channelFactory)
+		client.setPipelineFactory(new Factory(false, ssl))
+		client.connect(address).addListener(new ChannelFutureListener {
+			def operationComplete(future:ChannelFuture) {
+				if(future.isSuccess){
+					promise.success(NettyWire.this)
+				} else {
+					promise.failure(future.getCause)
+				}
+			}
+		})
+		(client, promise.future)
+	}
+
+	def listen(address:SocketAddress, ssl:Option[SSLContext]):(Bootstrap, Future[Wire]) = {
+		logger.trace(s"listen(${address.getName},$ssl)")
+		val promise = Promise[Wire]()
+		val channelFactory = new NioServerSocketChannelFactory()
+		val server = new ServerBootstrap(channelFactory)
+		server.setPipelineFactory(new Factory(true, ssl))
+		server.bindAsync(address).addListener(new ChannelFutureListener {
+			def operationComplete(future:ChannelFuture) {
+				if(future.isSuccess){
+					promise.success(NettyWire.this)
+				} else {
+					promise.failure(future.getCause)
+				}
+			}
+		})
+		(server, promise.future)
+	}
 }
--- src/src/test/scala/com/kazzla/asterisk/Sample.scala
+++ src/src/test/scala/com/kazzla/asterisk/Sample.scala
@@ -5,11 +5,67 @@
 */
 package com.kazzla.asterisk
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// Sample
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-/**
- * @author Takami Torao
- */
-class Sample {
+import java.util.concurrent.Executors
+import scala.io.Source
+import com.kazzla.asterisk.netty.Netty
+import java.net.InetSocketAddress
+import java.io.PrintWriter
+
+object Sample {
+	val executor = Executors.newCachedThreadPool()
+
+	object Node1 {
+
+		val ns = Node("NameServer").serve(new NameServer {
+			def lookup(name:String):Int = Session() match {
+				case Some(session) =>
+					val log = session.getRemoteInterface(classOf[LogServer])
+					log.info("hoge")
+
+					val pipe = session2.open(30, "hoge")
+					val out = new PrintWriter(pipe.out)
+					(0 until 10).foreach{ i => out.println(i) }
+					out.close()
+
+					100
+			}
+		}).runOn(executor).build()
+
+		Netty.listen(new InetSocketAddress(7777), ns)
+	}
+
+	object Node2 {
+
+		val logging = Node("LoggingServer").serve(new LogServer {
+			def error(msg:String) { Console.out.print(s"INFO : $msg\n") }
+			def info(msg:String)  { Console.out.print(s"ERROR: $msg\n") }
+			def dump(msg:String):Unit = Pipe.apply() match {
+				case Some(pipe) =>
+					Console.out.print(s"DUMP: $msg\n")
+					Source.fromInputStream(pipe.in).getLines().foreach { line =>
+						Console.out.println(line)
+					}
+			}
+		}).runOn(executor).build()
+
+		val session:Session = Netty.connect(new InetSocketAddress(7777), logging)
+
+		val ns = session1.getRemoteInterface(classOf[NameServer])
+		Console.println(ns.lookup("www.google.com"))
+	}
+
+}
+
+trait NameServer {
+	@Export(10)
+	def lookup(name:String):Int
+}
+
+trait LogServer {
+	@Export(10)
+	def info(msg:String):Unit
+	@Export(20)
+	def error(msg:String):Unit
+	@Export(30)
+	def dump(msg:String):Unit
 }
