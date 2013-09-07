5c299d30b877474a4f66f778c7c176ef0bc27e35
Takami Torao <koiroha@gmail.com>
2013-09-07 13:37:52 +0900

    add documentations

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-asterisk
+Asterisk\*
 ========
 
-Bidirectional RPC with async messaging protocol and implementations.
+Bidirectional RPC with asynchronous messaging protocol and implementations.
+
+Documents
+=========
+
+* [Motive](http://prezi.com/ia6rjvjrhe6d/asterisk-motivation/)
+* [Brain Storming](http://prezi.com/ktjdnfshx8dv/asterisk-brain-storming/)
+* [Introduction](docs/introduction.md)
+
+License
+=======
+[Apache License Version 2.0](LICENSE)
---
+++ src/src/test/scala/com/kazzla/asterisk/Sample.scala
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2013 koiroha.org.
+ * All sources and related resources are available under Apache License 2.0.
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+*/
+package com.kazzla.asterisk
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Sample
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/**
+ * @author Takami Torao
+ */
+class Sample {
+}
