ce50f8d451d503bff7b950bfb8ef2447ca97bb55
Takami Torao <koiroha@gmail.com>
2014-10-23 03:51:22 +0900

    modified

---
+++ ri/src/main/scala/org/asterisque/proxy/Server.scala
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2014 koiroha.org.
+ * All sources and related resources are available under Apache License 2.0.
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+*/
+package org.asterisque.proxy
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Server
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/**
+ * @author Takami Torao
+ */
+object Server {
+}
